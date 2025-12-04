1b931866d5f3b4720ca431e8ec6362cab814f254
Ryan Boswarthick <ryan.boswarthick@cefe.cnrs.fr>
2025-12-04 17:57:50 +0100

    edit gps mapview

--- R/gpsdata_withmapview.R
+++ R/gpsdata_withmapview.R
@@ -1,25 +1,19 @@
-library(dplyr)
+library(tidyverse)
 library(sf)
 library(mapview)
 library(lubridate)
 
 gpsdata_withmapview <- function(data) {
-  data_sf <- st_as_sf(data, coords = c(lon, lat), crs = 4326)
-  tracks_sf <- data_sf %>%
-    arrange(id, datetime) %>%
-    group_by(id) %>%
-    summarise(do_union = FALSE) %>%
+  data_sf <- sf::st_as_sf(data, coords = c("lon", "lat"), crs = 4326)
+  tracks_sf <- data_sf |>
+    arrange(id, datetime) |>
+    group_by(id) |>
+    summarise(do_union = FALSE) |>
     st_cast("LINESTRING")
   # Création de la carte mapview
-  map_obj <- mapview(data_sf, zcol = id, legend = TRUE, layer.name = "Points") +
+  map_obj <- mapview(data_sf, zcol = "id", legend = TRUE, layer.name = "Points") +
     mapview(tracks_sf, zcol = id, lwd = 2, legend = FALSE, layer.name = "Tracks")
-
-  return(list(
-    points_sf = data_sf,
-    tracks_sf = tracks_sf,
-    map = map_obj
-  ))
 }
 
-# result <- creer_map_gps(data)
-# result$map  # Affiche la carte
+# result
+gpsdata_withmapview(fusion_data)

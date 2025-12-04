d65a7391570dc347fd1be45d5412e961ee12c0dd
paulmiguet <paul.miguet@ofb.gouv.fr>
2025-12-04 16:10:06 +0100

    Ecriture de rbind_data dans combine_data

--- R/combine_data.R
+++ R/combine_data.R
@@ -1,6 +1,16 @@
 # Fonction pour fusionner les fichiers de données
-rbind_data <- function(chemin,motifFichier)
+rbind_data <- function(chemin,motif_fichier)
 {
+  files_list <- list.files(chemin,pattern='Bat')
+  output <- c()
+  if (length(files_list)==0) {
+    warning("Pas de fichiers dans le répertoire")
+  }
+  output <- c()
+  for (file in files_list) {
+    data <- readr::read_csv(chemin,file)
+    output <- rbind(outputs,datafile)
+  }
+  return(output)
 
-  return(mergeddata)
 }

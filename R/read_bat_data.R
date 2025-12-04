5bd0f3fd38780e3669b3038a384113f27b412edb
Noemie Collette <noemiecollette83@hotmail.com>
2025-12-04 16:49:19 +0100

    read_bat_data_final

--- R/read_bat_data.R
+++ R/read_bat_data.R
@@ -1,17 +1,23 @@
-#' Read bat list file
+#' Read all CSV files into a named list
 #'
 #' @description
-#' Cette fonction lit les documents .csv contenu dans le dossier "data".
-#' Les données sont téléchargées depuis la base de données "MoveBank"
+#' This function scans the `data/` directory for all `.csv` files,
+#' reads them as data frames, stores them in a named list,
+#' and assigns this list to the object `data` in the global environment.
 #'
-#' @param
-#' The path to the csv file.
+#' @details
+#' The names of the list elements correspond to the CSV file names
+#' without their extensions. The function returns a list in your Renvironment.
 #'
-#' @return
-#' A list with all .csv file
+#' @return A named list of data frames, returned invisibly.
+#'
+#' @examples
+#' \dontrun{
+#' read_bat_data()
+#' data  # Access the list created in the global environment
+#' }
 #'
 #' @export
-#'
 #'
 read_bat_data <- function() {
   path <- here::here("data")

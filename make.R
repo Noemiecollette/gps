b67ed69cd1d90d4b7270d877ba04acd9dd81db86
MarthaMacCall <martha.maccall@cefe.cnrs.fr>
2025-12-05 09:19:53 +0100

    modif

--- make.R
+++ make.R
@@ -2,7 +2,7 @@ devtools::install_deps(upgrade = "never")
 
 devtools::load_all()
 
-data<-read_bat_data()
-data_format<-format_gps_list(data)
-fusion_data<-rbind_data(data_format)
-plot_speed<-plot_speed(fusion_data)
+data <- read_bat_data()
+data_format <- format_gps_list(data)
+fusion_data <- rbind_data(data_format)
+plot_speed <- plot_speed(fusion_data)

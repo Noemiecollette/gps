67ee6e8bd6c445ceb572911018ea2eba3e2ffed8
Noemie Collette <noemiecollette83@hotmail.com>
2025-12-05 11:45:43 +0100

    presentation 1

--- make.R
+++ make.R
@@ -2,17 +2,8 @@ devtools::install_deps(upgrade = "never")
 
 devtools::load_all()
 
-data<-read_bat_data()
-data_format<-format_gps_list(data)
-fusion_data<-rbind_data(data_format)
-plot_speed <- plot_speed(fusion_data)
-plot_lonvstime <- plot_longitude_time(fusion_data)
-plot_latvstime <- plot_latitude_time(fusion_data)
-map<-gpsdata_withmapview(fusion_data)
-map
-save_html(map)
-
-
 targets::tar_make()
 
 targets::tar_visnetwork()
+
+quarto::quarto_render("Presentation.qmd")

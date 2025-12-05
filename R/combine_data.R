3a1d3679b9d8abeb0994a42673b50473a6e55efb
Noemie Collette <noemiecollette83@hotmail.com>
2025-12-05 09:42:48 +0100

    roxygen2 annotation

--- R/combine_data.R
+++ R/combine_data.R
@@ -1,17 +1,23 @@
-#' Combiner
+#' Bind a list of data frames
 #'
-#' @data_format_list
-#' liste de data formatees
-#' 
-#' output
+#' @description
+#' This function takes a list of data frames and binds them
+#' into a single data frame.
 #'
+#' @param data_format_list A list containing data frames to be combined.
 #'
-rbind_data <- function(data_format_list)
-{
+#' @return A data frame resulting from the binding of all
+#' elements in `data_format_list`.
+#'
+#' @examples
+#'
+#' @export
+#'
+rbind_data <- function(data_format_list) {
   output <- c()
   n <- length(data_format_list)
   for (i in 1:n) {
-    output <- rbind(output,data_format_list[[i]])
+    output <- rbind(output, data_format_list[[i]])
   }
   return(output)
 }
--- R/format_gps_list.R
+++ R/format_gps_list.R
@@ -1,27 +1,37 @@
-#' Formatage de data GPS
+#' Format a list of GPS data frames
 #'
-#' @description 
-#' Cette fonction formate le jeu de données, enlève les valeurs de NA et sélectionne les colonnes d'intérêt pour la suite des analyses.
+#' @description
+#' This function takes a list of raw GPS data frames and formats each one
+#' by converting timestamps, renaming relevant columns, and selecting
+#' only the required variables.
 #'
-#' @param file data une liste de .csv
+#' @param data A list of raw GPS data frames to be formatted.
 #'
-#' @return 
-#' 
+#' @return A list of formatted GPS data frames, each containing:
+#' `tag_id`, `datetime`, `lon`, `lat`, and `speed`.
+#'
+#' @details
+#' For each element of `data`, the function:
+#' - Converts `timestamp` to POSIXct (`datetime`).
+#' - Renames movement and location columns.
+#' - Keeps only standardised variables.
+#' - Removes possible NULL elements from the output list.
+#'
+#' @examples
+#'
 #' @export
-
 
 format_gps_list <- function(data) {
   data_format <- lapply(data, function(x) {
     x |>
       dplyr::mutate(
         datetime = as.POSIXct(timestamp, format = "%Y-%m-%d %H:%M:%S"),
-        speed    = `ground-speed`,
-        tag_id   = as.character(`tag-local-identifier`),
-        lon      = `location-long`,
-        lat      = `location-lat`
+        speed = `ground-speed`,
+        tag_id = as.character(`tag-local-identifier`),
+        lon = `location-long`,
+        lat = `location-lat`
       ) |>
       dplyr::select(tag_id, datetime, lon, lat, speed)
-    
   })
-    data_format <- data_format[!sapply(data_format, is.null)]
+  data_format <- data_format[!sapply(data_format, is.null)]
 }
